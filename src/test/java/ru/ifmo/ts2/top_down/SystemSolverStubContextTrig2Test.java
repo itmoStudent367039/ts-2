9269fd4bfbfd0e8eee67217f5e38315c112ac80b
Абдуллин Игорь <igor.adbullin.95@gmail.com>
2025-03-31 21:53:24 +0300

    lab-2: Done

--- src/test/java/ru/ifmo/ts2/top_down/SystemSolverStubContextTrig2Test.java
+++ src/test/java/ru/ifmo/ts2/top_down/SystemSolverStubContextTrig2Test.java
@@ -3,13 +3,11 @@ package ru.ifmo.ts2.top_down;
 import org.springframework.context.annotation.Import;
 import ru.ifmo.ts2.config.StubConfig;
 import ru.ifmo.ts2.config.StubSaverConfig;
-import ru.ifmo.ts2.config.trig.CosConfig;
 import ru.ifmo.ts2.config.trig.CotConfig;
 import ru.ifmo.ts2.config.trig.SecConfig;
 import ru.ifmo.ts2.config.trig.SinConfig;
 import ru.ifmo.ts2.config.trig.TanConfig;
 import ru.ifmo.ts2.functions.trig.Cos;
-import ru.ifmo.ts2.functions.trig.Tan;
 
 @Import(value = {
         StubConfig.class,
